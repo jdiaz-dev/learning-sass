68dd481c3d8b95468656a01c42dbfa6f0b94fa56
Jonathan Alejandro <lordgelsin26@gmail.com>
2020-10-09 13:53:08 -0200

    modules with sass finished

---
+++ 1 what is a preprocessor and what serve it.js
@@ -0,0 +1,15 @@
+/* 
+    --mucha gente lo confunde con framework
+*/
+
+/* 
+    PREPROCESADOR: es un programa que te permite generar CSS a partir de la sintaxix única del preprocesador
+    --añadiran algunas características que no existen en CSS como variables, mixins, funciones, selectores anidados
+    --añade características de un lenguaje de programación a un lenguaje de marcado
+    --esto hace a CSS mas legible y más fácil de mantener
+    --los más importantes son LESS y SASS
+    
+
+    --el fichero de SASS o LESS luego debemos compilarlo para convertirlo en CSS
+    -la diferencia entre un framework y un preprocesador es que un framework nos da más código desarrollado, mas un preprocesador solo añade unas cuantas funcionalidades a CSS nativo 
+*/
